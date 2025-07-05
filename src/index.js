c6d3c4c4499bcd79b9dbf1807b7ff2b635e968a1
Akhigbe Sadiq <akhigbesadiq@gmail.com>
2025-07-05 12:10:57 +0100

    worked on the workflow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
@@ -7,7 +8,9 @@ const app = express();
 // Throws an error if the PORT environment variable is missing.
 //
 if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
 }
 
 //
@@ -18,21 +21,27 @@ const PORT = process.env.PORT;
 //
 // Registers a HTTP GET route for video streaming.
 //
-app.get("/video", async (req, res) => { // Route for streaming video.
-    
-    const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
-    const stats = await fs.promises.stat(videoPath);
+app.get("/video", async (req, res) => {
+  // Route for streaming video.
 
-    res.writeHead(200, {
-        "Content-Length": stats.size,
-        "Content-Type": "video/mp4",
-    });
-    fs.createReadStream(videoPath).pipe(res);
+  const videoPath = path.join(
+    __dirname,
+    "../videos/SampleVideo_1280x720_1mb.mp4"
+  );
+  const stats = await fs.promises.stat(videoPath);
+
+  res.writeHead(200, {
+    "Content-Length": stats.size,
+    "Content-Type": "video/mp4",
+  });
+  fs.createReadStream(videoPath).pipe(res);
 });
 
 //
 // Starts the HTTP server.
 //
 app.listen(PORT, () => {
-    console.log(`Microservice online.`);
+  console.log(`Microservice online.`);
 });
+
+// Standard_D2s_v3
